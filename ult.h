00865056c5ca504c2e202a4a84d55ff374a13914
Tudor <114757688+TATGM@users.noreply.github.com>
2026-02-19 12:59:35 +0200

    Update ult.h

--- ult.h
+++ ult.h
@@ -9,12 +9,12 @@
 using ult_tid_t = int;
 
 // Thread API
-int ult_init(unsigned int quantum_us);
-int ult_create(ult_tid_t* thread_id, void* (*start_routine)(void*), void* argument);
-ult_tid_t ult_self();
-int ult_join(ult_tid_t thread_id, void** return_value);
-void ult_exit(void* return_value);
-int ult_yield();
+int ult_init(unsigned int quantum_us); // initializes the thread library and its internal structures
+int ult_create(ult_tid_t* thread_id, void* (*start_routine)(void*), void* argument); // creates a new thread
+ult_tid_t ult_self(); // returns the value of the currently executing thread.
+int ult_join(ult_tid_t thread_id, void** return_value); // suspends the calling thread until the specified thread_id terminates
+void ult_exit(void* return_value); // terminates the calling thread
+int ult_yield(); // voluntarily relinquishes the CPU
 
 // Mutex API
 struct ult_mutex {
@@ -24,8 +24,8 @@ struct ult_mutex {
   std::deque<ult_tid_t> waiters;
 };
 
-int ult_mutex_init(ult_mutex* mutex);
-int ult_mutex_lock(ult_mutex* mutex);
-int ult_mutex_unlock(ult_mutex* mutex);
+int ult_mutex_init(ult_mutex* mutex); // prepares the ult_mutex structure for use
+int ult_mutex_lock(ult_mutex* mutex); // // acquires the mutex or blocks the calling thread if it's already held
+int ult_mutex_unlock(ult_mutex* mutex); // releases the mutex and wakes up the next waiting thread
 
 #endif
